a3448003d4f90a1ffb7f9f5add422438c4cac35a
Fabian Johansson <fabulo1998@gmail.com>
2019-09-20 14:47:08 +0200

    Improved commenting

--- js/main.js
+++ js/main.js
@@ -36,30 +36,31 @@ class Question {
     }
 
     //Sköter all CSS som krävs för övergången mellan frågor
-    transitionToNextQuestion() {
+    transition() {
         let box = document.getElementById('questionBox');
         box.classList.add('hidden');
         box.classList.add('rainbow-boxshadow-flight');
+
         setTimeout(() => {
             box.classList.add('switch');
         }, 500);
+
         setTimeout(() => {
             box.classList.remove('hidden');
             box.classList.remove('rainbow-boxshadow-flight');
-
             box.classList.remove('switch');
         }, 600);
     };
 
     //Kör övergången mellan frågor och laddar in nästa fråga
-    nextQuestion() {
+    next() {
         if(player.currentQuestion >= this.questions.length - 1){
             this.player.currentQuestion = 0;
         } else {
             this.player.currentQuestion++;
         }
 
-        this.transitionToNextQuestion();
+        this.transition();
         setTimeout(() => {
             this.setQuestion(this.questions[this.player.currentQuestion])
         }, 500);
@@ -69,31 +70,37 @@ class Question {
     loadQuestions() {
         let req = new XMLHttpRequest();
 
+        //Sparar innehållet från JSON-filen i 'questions', ifall det misslyckas skrivs detta ut på skärmen
         req.onload = () => {
             if (req.status >= 200 && req.status < 300) {
                 console.log('Questions loaded');
                 this.questions = req.response;
-
             } else {
                 console.log('The request failed!');
                 q.currentQuestion.q.innerHTML = "Failed to get questions, try reloading page";
             }
 
+            //Gör om JSON-strängen till ett objekt och hämtar ut relevant data
             this.questions = JSON.parse(this.questions);
             this.questions = this.questions.questions;
 
+            //Sätter nuvarande fråga till första frågan i 'questions' och renderar den i DOM
             this.setQuestion(this.questions[this.player.currentQuestion]);
 
+            //Lägger en event listener på en knapp, som kör funktionen som laddar och renderar nästa fråga
             document.getElementById('nextQuestion').addEventListener('click', () => {
-                this.nextQuestion();
+                this.next();
             });
         };
-
+        
+        //Specar sökväg till JSON-filen och skickar requesten 
         req.open('GET', 'config/questions.json');
         req.send();
     }
 }
-const player = new Quiz(/*prompt('What is your name?')*/);
+
+
+const player = new Quiz(prompt('What is your name?'));
 const q = new Question(player);
 q.loadQuestions();
 

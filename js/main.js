aab7238c620a769b3dc289637d5a882fe72bf131
Fabian Johansson <fabulo1998@gmail.com>
2019-09-21 17:34:52 +0200

    Added function to check validity of answers, minified naming of methods and properties

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 class Quiz{
     constructor(name) {
+        //
         this.name = name || '';
         //Initierar frågeräknaren
-        this.currentQuestion = 0;
+        this.question = 0;
     }
 }
 
 class Question {
-    constructor(player) {
-        this.player = player;
+    constructor() {
+        this.player = [];
         //objekt som håller nuvarande laddade frågor
         this.questions = {};
         //Skapar ett objekt med referenser till HTML-elementen för frågan + svarsalternativ
         window.addEventListener('DOMContentLoaded', () => {
-            this.currentQuestion = {
-                q: document.getElementById('question'),
+            this.question = {
+                title: document.getElementById('question'),
                 options: [
                     document.getElementById('lbl1'),
                     document.getElementById('lbl2'),
@@ -27,12 +28,12 @@ class Question {
     }
 
     //Sätter vilken fråga som nu är aktiv, tar frågeobjekt som input
-    setQuestion(question) {
-        this.currentQuestion.q.innerHTML = question.title;
+    set(question) {
+        this.question.title.innerHTML = question.title;
         for (let i = 0; i < question.options.length; i++) {
-            q.currentQuestion.options[i].innerHTML = `<input type="checkbox" id="option${i + 1}">${question.options[i]}`;
+            q.question.options[i].innerHTML = `<input type="checkbox" id="option${i + 1}">${question.options[i]}`;
         }
-        this.currentQuestion.correct = question.correct;
+        this.question.correct = question.correct;
     }
 
     //Sköter all CSS som krävs för övergången mellan frågor
@@ -52,40 +53,47 @@ class Question {
         }, 600);
     };
 
+    checkAnswer() {
+        let answers = Array.from(document.getElementsByTagName('input')).map((answer) => {return answer.checked});
+        return this.question.correct.every((i) => {
+            return answers[i] == true;
+        });
+    }
+
     //Kör övergången mellan frågor och laddar in nästa fråga
     next() {
-        if(player.currentQuestion >= this.questions.length - 1){
-            this.player.currentQuestion = 0;
+        if(player.question >= this.questions.length - 1){
+            this.player[0].question = 0;
         } else {
-            this.player.currentQuestion++;
+            this.player[0].question++;
         }
+
+        console.log(this.checkAnswer());
 
         this.transition();
         setTimeout(() => {
-            this.setQuestion(this.questions[this.player.currentQuestion])
+            this.set(this.questions[this.player[0].question])
         }, 500);
     }
 
     //Laddar in frågorna från questions.json och parsear dem till objektet 'questions'
-    loadQuestions() {
+    load() {
         let req = new XMLHttpRequest();
 
         //Sparar innehållet från JSON-filen i 'questions', ifall det misslyckas skrivs detta ut på skärmen
         req.onload = () => {
             if (req.status >= 200 && req.status < 300) {
                 console.log('Questions loaded');
-                this.questions = req.response;
             } else {
                 console.log('The request failed!');
-                q.currentQuestion.q.innerHTML = "Failed to get questions, try reloading page";
+                q.question.title.innerHTML = "Failed to get questions, try reloading page";
             }
 
             //Gör om JSON-strängen till ett objekt och hämtar ut relevant data
-            this.questions = JSON.parse(this.questions);
-            this.questions = this.questions.questions;
+            this.questions = JSON.parse(req.response).questions;
 
             //Sätter nuvarande fråga till första frågan i 'questions' och renderar den i DOM
-            this.setQuestion(this.questions[this.player.currentQuestion]);
+            this.set(this.questions[this.player[0].question]);
 
             //Lägger en event listener på en knapp, som kör funktionen som laddar och renderar nästa fråga
             document.getElementById('nextQuestion').addEventListener('click', () => {
@@ -100,9 +108,10 @@ class Question {
 }
 
 
-const player = new Quiz(prompt('What is your name?'));
-const q = new Question(player);
-q.loadQuestions();
+const player = new Quiz('Fabian');
+const q = new Question();
+q.player.push(player);
+q.load();
 
 
 
